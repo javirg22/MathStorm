97a2aea3973db943919fadf469446913d762fde3
Gon <gonzalomartca@gmail.com>
2026-01-27 13:10:45 +0100

    Pantalla Splash

--- app/src/main/java/com/jagongui/mathstorm/SplashActivity.java
+++ app/src/main/java/com/jagongui/mathstorm/SplashActivity.java
@@ -1,24 +1,63 @@
 package com.jagongui.mathstorm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView logo = findViewById(R.id.logo);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
+
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                openApp();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
         });
+
+
+        logo.startAnimation(anim);
+
     }
+
+
+
+    private void openApp(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+
+        },250);
+
+
+    }
+
+
 }
